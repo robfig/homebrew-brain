49f39b0fffe5d6f6665f55a2c651f4123478a7dc
Rob Figueiredo <robfig@yext.com>
2019-05-26 10:05:13 -0400

    Update brain.rb

--- Formula/brain.rb
+++ Formula/brain.rb
@@ -104,8 +104,8 @@ end
 class Brain < Formula
   desc "Command-line interface to the Yext Brain"
   homepage "https://github.com/robfig/brain"
-  url "https://github.com/robfig/braindb/releases/download/0.5.1/brain", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
-  sha256 "55917b994f5835629d5e9698a1d74f12a6e243e740850c91ff523b3814241fc1"
+  url "https://github.com/robfig/braindb/releases/download/0.6/brain-darwin_amd64", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "d9f09759bc6dfc69ffdc01dcdfc281cb885dcdc194d5b6f23174c34cd13df2e6"
   head "https://github.com/robfig/braindb.git"
 
   def install
